37009e369645ba41cca6dc1854a67d77a86f6a25
Vladislav <112644268+JPRemizov@users.noreply.github.com>
2023-03-25 23:07:40 +0300

    Add

---
+++ AuthorizationWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Input;
+using TravelApp.DataSet1TableAdapters;
+using TravelApp.Windows;
+
+namespace TravelApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        AuthorizationTableAdapter authorizationAdapter = new AuthorizationTableAdapter();
+        EmployeeTableAdapter employeeTableAdapter = new EmployeeTableAdapter();
+        AdminWindow adminWindow = new AdminWindow();
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CloseAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void HideAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(LoginTextBox.Text) && !string.IsNullOrEmpty(PassTextBox.Password))
+            {
+                switch (FindUser(LoginTextBox.Text, PassTextBox.Password))
+                {
+                    case -1:
+                        {
+                            MessageBox.Show("Запись не найдена!");
+                            break;
+                        }
+                    case 1:
+                        {
+                            this.Hide();
+                            AdminWindow.authorizationWindow = this;
+                            adminWindow.Show();
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин и пароль!");
+            }
+        }
+        private int FindUser(string login, string password)
+        {
+            DataTable authTable = authorizationAdapter.GetData();
+            for (int i = 0; i < authTable.Rows.Count; i++)
+            {
+                if (login == (string)authTable.Rows[i][2])
+                {
+                    if (password == (string)authTable.Rows[i][3])
+                    {
+                        return Convert.ToInt32(authTable.Rows[i][1]);
+                    }
+                }
+
+            }
+            return -1;
+        }
+    }
+}
---
+++ Windows/AdminWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TravelApp.Pages;
+
+namespace TravelApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public static AuthorizationWindow authorizationWindow;
+        public AdminWindow()
+        {
+            InitializeComponent();
+            Table1.Focus();
+        }
+
+        private void HideAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            authorizationWindow.Show();
+        }
+
+        private void Table1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            AdminFrame.Content = new EmployeePage();
+        }
+
+        private void Table1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AdminFrame.Content = new EmployeePage();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
